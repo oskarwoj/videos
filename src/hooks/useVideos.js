f2a8c818adcfb460de46ba86650e4045c26c551a
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-08-05 13:46:20 +0200

    hooks added

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import youtube from "../apis/youtube";
+
+const useVideos = (defaultSearchTerm) => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    search(defaultSearchTerm);
+  }, [defaultSearchTerm]);
+
+  const search = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        type: "video",
+        key: "AIzaSyCN6e98GjAfa_d4E4mISSn8uCP6g5b1DvE",
+      },
+    });
+
+    setVideos(response.data.items);
+  };
+
+  return [videos, search];
+};
+
+export default useVideos;

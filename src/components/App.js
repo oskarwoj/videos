4289650d58cc0909b0532d049018a34007755644
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-08-06 10:05:23 +0200

    default video updated

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import useVideos from "../hooks/useVideos";
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
-  const [videos, search] = useVideos("buildings");
+  const [videos, search] = useVideos("euro 2016");
 
   useEffect(() => {
     setSelectedVideo(videos[0]);
